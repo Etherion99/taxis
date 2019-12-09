82b30e3e2a38c9fd73eed7b3bb84b8a76bf9f9da
Sebastian Trujillo <juanstt99@gmail.com>
2019-12-09 10:16:10 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Taxis uu2
+                    Taxis uu
                 </div>
 
                 <div class="links">
